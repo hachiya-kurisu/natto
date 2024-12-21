41a681a0ff63bbdc2140d9130418bd2eb317c035
蜂谷栗栖 <kurisu@blekksprut.net>
2024-12-21 21:17:11 +0900

    a few tests

---
+++ natto_test.go
@@ -0,0 +1,38 @@
+package natto
+
+import (
+	"net/url"
+	"testing"
+	"io/ioutil"
+)
+
+var c Capsule
+
+func init() {
+	c.Writer = ioutil.Discard
+}
+
+func TestGemtext(t *testing.T) {
+	url, _ := url.Parse("gemini://test/README.gmi")
+	c.Validate(url)
+	if c.Request(url) != Ok {
+		t.Errorf("request to this file failed")
+	}
+}
+
+func TestDefaultMime(t *testing.T) {
+	url, _ := url.Parse("gemini://test/natto_test.go")
+	c.Validate(url)
+	if c.Request(url) != Ok {
+		t.Errorf("request to this file failed")
+	}
+}
+
+func TestMissingFile(t *testing.T) {
+	url, _ := url.Parse("gemini://test/eyyyyy")
+	c.Validate(url)
+	if c.Request(url) != Oops {
+		t.Errorf("this file shouldn't even be here today")
+	}
+}
+
